ea6180096ab2717bc1239d7ccb20c4e3082c467a
JoelsonCRJ <joelsonrocha.eng@gmail.com>
2019-05-07 14:05:15 -0300

    atualizando repositorio

--- 01-EMG/main.py
+++ 01-EMG/main.py
@@ -42,52 +42,70 @@ b, a = signal.butter(5, [10/500, 450/500], 'bandpass')
 Y_butter = lfilter(b, a,data)
 N=len(Y_butter)
 t=N*T
+YN=scipy.fftpack.fft(data)
+#codigo para plotar fft data
+P2N=abs(YN/N)
+an=int ((N/2)+1)
+P1N=P2N[2:an]
+P1N[2:N-1]=2*P1N[2:N-1]
+fN = (Fs/N)*np.arange(0,(N/2),1)
+plt.figure(1)
 
+plt.xlabel('Frequência (Hz)')
+plt.ylabel('|EMG(t)|')
+plt.plot(fN[0:len(fN)-1],P1N)
+plt.legend(['FFT do sinal EMG antes da filtragem'],loc='best')
+###################################
 Y=scipy.fftpack.fft(Y_butter)
-#codigo para plotar fft
-# P2=abs(Y/N)
-# a=int ((N/2)+1)
-# P1=P2[2:a]
-# P1[2:N-1] = 2*P1[2:N-1]
-# f = (Fs/N)*np.arange(0,(N/2),1)
-# plt.plot(f[0:len(f)-1],P1)-1
-# plt.show()
+#codigo para plotar fft butter
+P2=abs(Y/N)
+a=int ((N/2)+1)
+P1=P2[2:a]
+P1[2:N-1] = 2*P1[2:N-1]
+f = (Fs/N)*np.arange(0,(N/2),1)
+plt.figure(2)
+
+plt.plot(f[0:len(f)-1],P1,'r')
+plt.legend(['FFT do sinal EMG após filtragem'],loc='best')
+plt.xlabel('Frequência (Hz)')
+plt.ylabel('|EMG*(t)|')
+plt.show()
+
+
+
 a=scipy.ifft(Y)
 Inv = scipy.ifft(Y)
-ind = detect_peaks(Inv,mph=0, mpd=(750),show=True)
+ind = detect_peaks(Inv,mph=0, mpd=(750),show=None)
 Inv=interp_peaks(ind,Inv,50)
-print(ind)
+#print(ind)
 
 
-plt.figure(1)
-plt.plot(time,data,'b')
-plt.legend(['Sinal original'],loc='best')
-plt.ylim(440, 560)
-plt.xlim(0,60000)
-plt.xlabel('tempo (ms)')
-plt.ylabel('Valor de sinal quantizado - 12bits')
+# plt.figure(1)
+# plt.plot(time,data,'b')
+# plt.legend(['Sinal original'],loc='best')
+# plt.ylim(440, 560)
+# plt.xlim(0,60000)
+# plt.xlabel('tempo (ms)')
+# plt.ylabel('Valor de sinal quantizado - 12bits')
 
-plt.figure(2)
-plt.plot(time,a,'r')
-plt.legend(['Sinal filtrado - Butter 5ª ordem'],loc='best')
-plt.ylim(-60, 60)
-plt.xlim(0,60000)
+# plt.figure(2)
+# plt.plot(time,a,'r')
+# plt.legend(['Sinal filtrado - Butter 5ª ordem'],loc='best')
+# plt.ylim(-60, 60)
+# plt.xlim(0,60000)
 
-plt.xlabel('tempo (ms)')
-plt.ylabel('Valor de sinal quantizado - 12bits')
-# plt.savefig('a.ps', dpi=95, facecolor=None, edgecolor=None,
-#         orientation='portrait', papertype=None, format=None,
-#         transparent=False, bbox_inches=None, pad_inches=0.1,
-#         frameon=None, metadata=None)
+# plt.xlabel('tempo (ms)')
+# plt.ylabel('Valor de sinal quantizado - 12bits')
 
-plt.figure(3)
-plt.plot(time,a,'b',label='Sinal filtrado com ECG')
-plt.plot(time,Inv,'r',label='Sinal filtrado sem ECG')
-plt.legend(['Sinal filtrado com ECG','Sinal filtrado sem ECG'],loc='best')
-plt.xlabel('tempo (ms)')
-plt.ylabel('Valor de sinal quantizado - 12bits')
-plt.ylim(-60, 60)
-plt.xlim(0,60000)
+
+# plt.figure(3)
+# plt.plot(time,a,'b',label='Sinal filtrado com ECG')
+# plt.plot(time,Inv,'r',label='Sinal filtrado sem ECG')
+# plt.legend(['Sinal filtrado com ECG','Sinal filtrado sem ECG'],loc='best')
+# plt.xlabel('tempo (ms)')
+# plt.ylabel('Valor de sinal quantizado - 12bits')
+# plt.ylim(-60, 60)
+# plt.xlim(0,60000)
 
 plt.show()
 
