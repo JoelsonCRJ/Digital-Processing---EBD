320f44776903f8be135d016e1091599ce5ab9294
JoelsonCRJ <joelsonrocha.eng@gmail.com>
2019-05-15 15:07:27 -0300

    up

--- 03- sEMG/main.py
+++ 03- sEMG/main.py
@@ -34,7 +34,7 @@ emg_signal_9 = np.array((data_EMG_signals[:,8]))
 emg_signal_10 = np.array((data_EMG_signals[:,9]))
 
 
-print(emg_signal_10)
+print(classes)
 secoes_vector = np.zeros(len(tempo))
 secoes_classe = np.zeros(len(tempo))
 print(len(classes))
@@ -49,64 +49,64 @@ for j in range(0,440):
 
 
 
-plt.figure(1)
-#plot canais
+# plt.figure(1)
+# #plot canais
 
-plt.subplot(13,1,1)
-plt.plot(tempo,secoes_vector)
-plt.xlim(0,1260450)
+# plt.subplot(13,1,1)
+# plt.plot(tempo,secoes_vector)
+# plt.xlim(0,1260450)
 
-plt.subplot(13,1,2)
-plt.plot(tempo,secoes_classe,'r')
-plt.xlim(0,1260450)
+# plt.subplot(13,1,2)
+# plt.plot(tempo,secoes_classe,'r')
+# plt.xlim(0,1260450)
 
-plt.subplot(13,1,3)
-plt.plot(tempo,data_knee_angle)
-plt.xlim(0,1260450)
+# plt.subplot(13,1,3)
+# plt.plot(tempo,data_knee_angle)
+# plt.xlim(0,1260450)
 
-plt.subplot(13,1,4)
-plt.plot(tempo,emg_signal_1)
-plt.xlim(0,1260450)
+# plt.subplot(13,1,4)
+# plt.plot(tempo,emg_signal_1)
+# plt.xlim(0,1260450)
 
-plt.subplot(13,1,5)
-plt.plot(tempo,emg_signal_2)
-plt.xlim(0,1260450)
+# plt.subplot(13,1,5)
+# plt.plot(tempo,emg_signal_2)
+# plt.xlim(0,1260450)
 
-plt.subplot(13,1,6)
-plt.plot(tempo,emg_signal_3)
-plt.xlim(0,1260450)
+# plt.subplot(13,1,6)
+# plt.plot(tempo,emg_signal_3)
+# plt.xlim(0,1260450)
 
-plt.subplot(13,1,7)
-plt.plot(tempo,emg_signal_4)
-plt.xlim(0,1260450)
+# plt.subplot(13,1,7)
+# plt.plot(tempo,emg_signal_4)
+# plt.xlim(0,1260450)
 
-plt.subplot(13,1,8)
-plt.plot(tempo,emg_signal_5)
-plt.xlim(0,1260450)
+# plt.subplot(13,1,8)
+# plt.plot(tempo,emg_signal_5)
+# plt.xlim(0,1260450)
 
-plt.subplot(13,1,9)
-plt.plot(tempo,emg_signal_6)
-plt.xlim(0,1260450)
+# plt.subplot(13,1,9)
+# plt.plot(tempo,emg_signal_6)
+# plt.xlim(0,1260450)
 
-plt.subplot(13,1,10)
-plt.plot(tempo,emg_signal_7)
-plt.xlim(0,1260450)
+# plt.subplot(13,1,10)
+# plt.plot(tempo,emg_signal_7)
+# plt.xlim(0,1260450)
 
-plt.subplot(13,1,11)
-plt.plot(tempo,emg_signal_8)
-plt.xlim(0,1260450)
+# plt.subplot(13,1,11)
+# plt.plot(tempo,emg_signal_8)
+# plt.xlim(0,1260450)
 
-plt.subplot(13,1,12)
-plt.plot(tempo,emg_signal_9)
-plt.xlim(0,1260450)
+# plt.subplot(13,1,12)
+# plt.plot(tempo,emg_signal_9)
+# plt.xlim(0,1260450)
 
-plt.subplot(13,1,13)
-plt.plot(tempo,emg_signal_10)
-plt.xlim(0,1260450)
-# #plt.suptitle(str(classes))
+# plt.subplot(13,1,13)
+# plt.plot(tempo,emg_signal_10)
+# plt.xlim(0,1260450)
+# # #plt.suptitle(str(classes))
 
 
-plt.show()
+# plt.show()
 
 
 
