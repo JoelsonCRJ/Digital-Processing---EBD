dd895ad43bb94fcd46e204ec4dbb89c4c5abda18
joelsoncrj <joelsonrocha.eng@gmail.com>
2019-05-17 22:18:58 -0300

    corrigindo plot de amostras.

--- 02-EMG/main3.py
+++ 02-EMG/main3.py
@@ -9,7 +9,7 @@ from scipy import signal
 from sklearn import svm
 from mlxtend.plotting import plot_decision_regions
 from six.moves import cPickle as pickle
-from sklearn.decomposition import PCA
+
 
 def save_dict(di_, filename_):
     with open(filename_, 'wb') as f:
@@ -35,8 +35,7 @@ def plot_session(musculos, intervalos, labels):
 
 
 def descript_vectors(musc, intervalos,labels):
-        M=np.array([])
-        L=np.array([])
+        global M,L
         global V1_training,V2_training,V3_training,V4_training,V5_training,V6_training
         for i in range(0,len(labels)):
                 v=np.array([])
@@ -44,54 +43,44 @@ def descript_vectors(musc, intervalos,labels):
                 intervalo_final = intervalos[i][1]
                 for j in range(0,11):
                         v=np.append(v,musc[j][intervalo_inicial:intervalo_final])
-                if(labels[i]==1):
-                        V1_training=np.concatenate((V1_training,v),axis=0)
-                elif(labels[i]==2):
-                        V2_training=np.concatenate((V2_training,v),axis=0)
-                elif(labels[i]==3):
-                        V3_training=np.concatenate((V3_training,v),axis=0)
-                elif(labels[i]==4):
-                        V4_training=np.concatenate((V4_training,v),axis=0)
-                elif(labels[i]==5):
-                        V5_training=np.concatenate((V5_training,v),axis=0)
-                else:
-                        V6_training=np.concatenate((V6_training,v),axis=0)
-                # M=np.concatenate((M,v),axis=0)   
-                # L=np.append(L,labels[i])
+                
+                M=np.concatenate((M,v),axis=0)   
+                L=np.append(L,labels[i])
                 #print((M.size)/800)
-        # M=M.reshape(44,8800)
-        # return M,L
         
-
+        # return M,L
+M=np.array([])
+L=np.array([])
 
 sessions = load_dict('/home/familia/Documents/Digital-Processing-EBD-UFES-2019.1/02-EMG/data.pkl')
 #plot_session(sessions['1'][0].T[0:11],sessions['1'][1],sessions['1'][2])
 
 
-V1_training=np.array([])
-V2_training=np.array([])
-V3_training=np.array([])
-V4_training=np.array([])
-V5_training=np.array([])
-V6_training=np.array([])
+
 
 
 
 #treinamento
-for i in range(1,10):
+for i in range(1,11):
         descript_vectors(sessions['{}'.format(i)][0].T[0:11],sessions['1'][1],sessions['1'][2])
+        #print(M.shape)
+M=M.reshape(440,8800)
 
-description_vector_size=8800
-V1_training=V1_training.reshape(int((V1_training.size)/description_vector_size),description_vector_size)
-V2_training=V2_training.reshape(int((V2_training.size)/description_vector_size),description_vector_size)
-V3_training=V3_training.reshape(int((V3_training.size)/description_vector_size),description_vector_size)
-V4_training=V4_training.reshape(int((V4_training.size)/description_vector_size),description_vector_size)
-V5_training=V5_training.reshape(int((V5_training.size)/description_vector_size),description_vector_size)
-V6_training=V6_training.reshape(int((V6_training.size)/description_vector_size),description_vector_size)
+# print(L.size)
+# print(M.shape)
+pca =decomposition.PCA(n_components=2)
+pca.fit(M)
+X = pca.transform(M)
+Y=L.T
+plt.scatter(X[:, 0], X[:, 1],c=L.T, edgecolor='none', alpha=0.5,cmap=plt.cm.get_cmap('Spectral', 6))
+plt.grid()
+plt.colorbar()
+plt.show()
+#aplicando SVM
 
 
-#aplicando PCA
-print(V4_training.shape)
+
+
 
 
 
